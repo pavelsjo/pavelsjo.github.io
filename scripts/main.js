f408300b3585f4fc3b99157a79d550fe250a1e6a
pavelsjo <pavels.jo@gmail.com>
2020-10-28 11:38:07 -0300

    Header stable

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+/* Card */
+
 let imageCard = document.querySelector('img');
 
 
@@ -11,3 +13,29 @@ let imageCard = document.querySelector('img');
 // };
 
 imageCard.setAttribute('src','blog/01-keep-last-date-oac-df/images/step-0'+ String(0) + '.png')
+
+/* Header */
+
+function openCategory(cityName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(cityName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click(); 
