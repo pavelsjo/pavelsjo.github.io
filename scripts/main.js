f708882e4fdb4b435ff68898f4b34e00d282e6a3
pavelsjo <pavels.jo@gmail.com>
2020-10-27 22:44:42 -0300

    Full content and resumen version init"

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+let imageCard = document.querySelector('img');
+
+
+// setTimeout(myFunction, 10000);
+
+// for (let index = 0; index <= 5; index++) {
+//     function myFunction() {
+//         imageCard.setAttribute('src','blog/01-keep-last-date-oac-df/images/step-0'+ String(index) + '.png')
+//     }
+        
+// };
+
+imageCard.setAttribute('src','blog/01-keep-last-date-oac-df/images/step-0'+ String(0) + '.png')
